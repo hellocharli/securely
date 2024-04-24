766052709b2e8fad038cf82dfaa5c2cb28a587f2
charlie lowe <111490544+hellocharli@users.noreply.github.com>
2024-04-23 21:49:14 -0700

    Add interactive routing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,15 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+
+    app.get(":route") { req async throws -> String in
+
+        guard let route = req.parameters.get("route")
+        else {
+            throw Abort(.badRequest)
+        }
+
+        return "Your route is: \(route)"
+
+    }
 }
