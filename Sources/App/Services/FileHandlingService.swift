f6abdb262477c0b24747c1494b1658607e3e510e
charlie lowe <111490544+hellocharli@users.noreply.github.com>
2024-04-24 10:18:51 -0700

    Update FileHandlingService to create dirs. Update gitignore

--- Sources/App/Services/FileHandlingService.swift
+++ Sources/App/Services/FileHandlingService.swift
@@ -2,9 +2,12 @@ import Vapor
 
 struct FileHandlingService {
     let directory = DirectoryConfiguration.detect().workingDirectory
+    let contentDirectory = "Content"
 
     func retrieveFile(named hash: String, on req: Request) -> EventLoopFuture<String> {
-        let fileURL = URL(fileURLWithPath: directory).appendingPathComponent("Files").appendingPathComponent(hash)
+        let fileURL = URL(fileURLWithPath: directory)
+                        .appendingPathComponent(contentDirectory)
+                        .appendingPathComponent(hash)
         do {
             let content = try String(contentsOf: fileURL, encoding: .utf8)
             return req.eventLoop.makeSucceededFuture(content)
@@ -14,8 +17,17 @@ struct FileHandlingService {
     }
 
     func createOrUpdateFile(named hash: String, content: String, on req: Request) -> EventLoopFuture<Void> {
-        let fileURL = URL(fileURLWithPath: directory).appendingPathComponent("Files").appendingPathComponent(hash)
+        let fileURL = URL(fileURLWithPath: directory)
+                      .appendingPathComponent(contentDirectory)
+                      .appendingPathComponent(hash)
+        
+        let fileManager = FileManager.default
+        let directoryPath = fileURL.deletingLastPathComponent()
+
         do {
+            if !fileManager.fileExists(atPath: directoryPath.path) {
+                try fileManager.createDirectory(atPath: directoryPath.path, withIntermediateDirectories: true, attributes: nil)
+            }
             try content.write(to: fileURL, atomically: true, encoding: .utf8)
             return req.eventLoop.makeSucceededFuture(())
         } catch {
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,28 +5,19 @@ func routes(_ app: Application) throws {
     let hashingService = HashingService()
     let fileHandlingService = FileHandlingService()
 
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-
     app.get(":route") { req -> EventLoopFuture<String> in
         guard let route = req.parameters.get("route") else {
             throw Abort(.badRequest)
         }
 
         let hash = hashingService.generateSHA256Hash(for: route)
+        print("Hashed URL: \(hash)")
         return fileHandlingService.retrieveFile(named: hash, on: req)
             .flatMapError { error in
                 if let abortError = error as? Abort, abortError.status == .notFound {
-                    // Handle not found error by creating a new file with empty content
                     return fileHandlingService.createOrUpdateFile(named: hash, content: "Initial content", on: req)
                         .flatMap { _ in fileHandlingService.retrieveFile(named: hash, on: req) }
                 } else {
-                    // If the error is not a 'notFound', propagate it
                     return req.eventLoop.makeFailedFuture(error)
                 }
             }
